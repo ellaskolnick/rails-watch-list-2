194e5d11c6ccfd2c310b32cb9fd161f5adc1e38e
Ella Skolnick <ellaskolnick@me.com>
2022-05-26 23:44:05 +0100

    updates

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,33 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    mark = params[:bookmark]
+    @bookmark.list = @list
+    @movie = Movie.find(mark[:movie_id])
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to @list, notice: 'Bookmark Added'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    @list = @bookmark.list
+    redirect_to list_path(@list), notice: 'Bookmark Deleted'
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :select_value, :movie)
+  end
 end
